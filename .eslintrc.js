0f72ddbb75c7b62f994cc8407943e5e6443e9769
shehabkhalid <shehabkhalid1999@gmail.com>
2022-02-05 20:51:27 +0200

    Starter

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'import'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'import/extensions': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+};
